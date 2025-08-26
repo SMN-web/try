603ca49738859e0085dcbfad37895b1a58cf48cd
SMN-web <nafil.8895.s@gmail.com>
2025-08-26 15:59:17 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyDtZnYYDb5TR01G6zsCtrF0HBR6pnQ2Beg",
@@ -7,23 +7,13 @@ firebase.initializeApp({
     projectId: "general-68ca7",
     storageBucket: "general-68ca7.firebasestorage.app",
     messagingSenderId: "674522865143",
-    appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2"
+    appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2",
+    measurementId: "G-6L0DXHGCBE"
 });
 
 const messaging = firebase.messaging();
 
-// Background or webpage not active → show native notification
-messaging.onBackgroundMessage(payload => {
-  console.log('[firebase-messaging-sw.js] Background message:', payload);
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || '/icon.png',
-    data: payload.data || {}
-  });
-});
-
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+messaging.onBackgroundMessage(function(payload) {
+  const { title, body } = payload.notification || {};
+  self.registration.showNotification(title || "Notification", { body });
 });

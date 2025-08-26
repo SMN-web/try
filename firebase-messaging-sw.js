c1cee64aa56916ce1d351ff33ba023813b782435
SMN-web <nafil.8895.s@gmail.com>
2025-08-26 15:08:10 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+// Place at /firebase-messaging-sw.js in your web root!
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+// Use your public Firebase config
+firebase.initializeApp({
+  apiKey: "AIzaSyDtZnYYDb5TR01G6zsCtrF0HBR6pnQ2Beg",
+    authDomain: "general-68ca7.firebaseapp.com",
+    projectId: "general-68ca7",
+    storageBucket: "general-68ca7.firebasestorage.app",
+    messagingSenderId: "674522865143",
+    appId: "1:674522865143:web:c4ec47f2e370c33c3ca2f2",
+    measurementId: "G-6L0DXHGCBE"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const { title, body } = payload.notification || {};
+  self.registration.showNotification(title || "Notification", { body });
+});
